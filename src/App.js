075c17bf14916689e627fb62178c352b4893fc73
Rodolpho Bravo <bravorodolpho@gmail.com>
2021-11-15 14:57:06 -0400

    Add Anchor and write Program test (Programs are sorta like contracts on Ethereum)

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { useState, useEffect } from "react"
 import twitterLogo from "./assets/twitter-logo.svg"
 import "./App.css"
 
+const TEST_GIFS = [
+  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
+  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
+]
+
 // Constants
 const TWITTER_HANDLE = "rodolphobravo"
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null)
+  const [inputValue, setInputValue] = useState("")
+  const [gifList, setGifList] = useState([])
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window
@@ -41,6 +51,19 @@ const App = () => {
     }
   }
 
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log("Gif link:", inputValue)
+    } else {
+      console.log("Empty input. Try again.")
+    }
+  }
+
+  const onInputChange = (event) => {
+    const { value } = event.target
+    setInputValue(value)
+  }
+
   const renderNotConnectedContainer = () => (
     <button
       className="cta-button connect-wallet-button"
@@ -50,6 +73,34 @@ const App = () => {
     </button>
   )
 
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          sendGif()
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter gif link!"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button type="submit" className="cta-button submit-gif-button">
+          Submit
+        </button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected()
@@ -57,6 +108,17 @@ const App = () => {
     window.addEventListener("load", onLoad)
     return () => window.removeEventListener("load", onLoad)
   }, [])
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("Fetching GIF list...")
+
+      // Call Solana program here.
+
+      // Set state
+      setGifList(TEST_GIFS)
+    }
+  }, [walletAddress])
 
   return (
     <div className="App">
@@ -69,6 +131,7 @@ const App = () => {
           </p>
           {/* Add the condition to show this only if we don't have a wallet address */}
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
